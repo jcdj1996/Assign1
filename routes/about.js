34983d5a70414e08bbd0a46afe8b8ad6cf75ae1d
jcdj1996 <jcdj1996@hotmail.com>
2015-10-14 03:19:59 -0400

    Adding links and about pt2

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'James Jennings' });
+  res.render('about', { title: 'About | James Jennings' });
 });
 
 module.exports = router;
